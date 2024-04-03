f424c788054d74cc4a306c0ac727cc716e991225
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-04-03 20:18:38 +0200

    spring boot project setup

---
+++ project_api/src/test/java/com/olos/project_api/ProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olos.project_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
